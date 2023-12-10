ff710a16b3b87337fa95cd5f5fb5bdd619252027
Pritesh Vardekar <priteshv.264@gmail.com>
2023-12-10 17:12:47 +0530

    js changes

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let con = document.querySelector("#container");
 let heart = document.querySelector(".whiteheart");
 let engage = document.querySelector(".engage");
 let hero = document.getElementById("hero");
- var click =1;
+ var likeclick =1;
+ var heartclick =1;
    
 function heartfunc(){
    heart.style.transform='translate(-50%,-50%) scale(1)';
@@ -14,20 +15,26 @@ function heartfunc(){
     heart.style.transform='translate(-50%,-50%) scale(0)';
    },2000)
 }
-con.addEventListener("dblclick",heartfunc)
+con.addEventListener("dblclick",heartfunc);
+con.addEventListener("dblclick",()=>{
+   if(heartclick==1){
+      likefunc();
+      heartclick =0;
+   }
+});
 
 function likefunc(){
-   if(click==1){
+   if(likeclick==1){
       heartfunc();
    hero.classList.add('ri-heart-3-fill','redheart');
    hero
    hero.classList.remove('ri-heart-line');
-   click=0;
+   likeclick=0;
    }
-   else if(click==0){
+   else if(likeclick==0){
       hero.classList.remove('ri-heart-3-fill','redheart');
-      hero
       hero.classList.add('ri-heart-line');
-      click=1;
+      likeclick=1;
+      heartclick=1;
    }
 }
