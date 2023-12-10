10a7a8ec2036132820292209b74cc6c0f71b575c
Pritesh Vardekar <priteshv.264@gmail.com>
2023-12-10 16:58:54 +0530

    added like feature

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let con = document.querySelector("#container");
-let heart = document.querySelector("i");
-
-con.addEventListener("dblclick",()=>{
+let heart = document.querySelector(".whiteheart");
+let engage = document.querySelector(".engage");
+let hero = document.getElementById("hero");
+ var click =1;
+   
+function heartfunc(){
    heart.style.transform='translate(-50%,-50%) scale(1)';
-   heart.style.transition='transform 0.4s ease';
    heart.style.opacity="0.8";
    setTimeout(function(){
         heart.style.opacity="0";
@@ -11,4 +13,21 @@ con.addEventListener("dblclick",()=>{
    setTimeout(function(){
     heart.style.transform='translate(-50%,-50%) scale(0)';
    },2000)
-})
+}
+con.addEventListener("dblclick",heartfunc)
+
+function likefunc(){
+   if(click==1){
+      heartfunc();
+   hero.classList.add('ri-heart-3-fill','redheart');
+   hero
+   hero.classList.remove('ri-heart-line');
+   click=0;
+   }
+   else if(click==0){
+      hero.classList.remove('ri-heart-3-fill','redheart');
+      hero
+      hero.classList.add('ri-heart-line');
+      click=1;
+   }
+}
